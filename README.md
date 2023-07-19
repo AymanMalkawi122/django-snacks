e9087009d81a80bbc6bde2388d7f6f9d1e30a5e8
AymanMalkawi122 <malkawiayman63@gmail.com>
2023-07-20 01:14:43 +0300

    fix task

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# django-snacks
+# LAB - Class 26
+
+Project: django snacks
+
+Author: Ayman Malkawi
+
+## Setup
+
+run ```pip install -r requirments.txt```
+
+## Tests
+
+run ```python manage.py test```
+
+## How to initialize/run your application
+
+run ```python manage.py runserver```
--- snaks/tests.py
+++ snaks/tests.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 class TestHome(SimpleTestCase):
     def test_status_code(self):
         url = reverse('home')
-        print("\nhome URL:",url)
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
@@ -13,7 +12,7 @@ class TestHome(SimpleTestCase):
     def test_home_page_templates(self):
         url = reverse('home')
         response = self.client.get(url)
-        self.assertTemplateUsed(response,  'base.html')
+        self.assertTemplateUsed(response, 'base.html')
 
 
 class TestAbout(SimpleTestCase):
